1fd0f943d7dfdd60fb324cdc081ac3cef52f19bf
anthony ren <anthony__ren@163.com>
2019-10-08 15:20:17 +0800

    springboot方式引入dubbo

--- src/main/java/com/anthony/service/DubboServiceApplication.java
+++ src/main/java/com/anthony/service/DubboServiceApplication.java
@@ -1,5 +1,6 @@
 package com.anthony.service;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.ImportResource;
  * @author mgdm
  */
 @SpringBootApplication
-@ImportResource(value = {"classpath:dubbo-providers.xml"})
+@EnableDubbo
 public class DubboServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/anthony/service/demoservice/DemoService.java
+++ src/main/java/com/anthony/service/demoservice/DemoService.java
@@ -1,11 +1,13 @@
 package com.anthony.service.demoservice;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.anthony.service.demointerface.DemoInterface;
 
 /**
  * @Author: renyiran
  * @Date: 2019/9/24 15:53
  */
+@Service(version = "1.0.0")
 public class DemoService implements DemoInterface {
     @Override
     public String sayHello(String str) {
