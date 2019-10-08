88b260de99912c15bea3c348050256272de54fa9
anthony ren <anthony__ren@163.com>
2019-10-08 14:56:29 +0800

    试试

--- src/main/java/com/anthony/service/demoservice/DemoService.java
+++ src/main/java/com/anthony/service/demoservice/DemoService.java
@@ -1,13 +1,11 @@
 package com.anthony.service.demoservice;
 
 import com.anthony.service.demointerface.DemoInterface;
-import org.springframework.stereotype.Service;
 
 /**
  * @Author: renyiran
  * @Date: 2019/9/24 15:53
  */
-@Service
 public class DemoService implements DemoInterface {
     @Override
     public String sayHello(String str) {
